e82ff592c432746a772992e49c53d1137d3a1762
Abdoul Aziz BALDE <baldaazi99@gmail.com>
2025-04-19 14:34:15 +0200

    add logo

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,3 +1,15 @@
-<header class="text-center text-blue-200">
-    Welcome to svelte
+<script lang="ts">
+    import {page} from '$app/state'
+    import logo from '$assets/images/logo.svg'
+</script>
+
+<header class="flex bg-teal-600 shadow backdrop-blur-md">
+    <a class="flex items-center">
+        <img src={logo} alt="SvelteKit" class="w-10 h-10 text-gray-500/60"/>
+        <h1 class="text-xl font-semibold bg-linear-to-r from-gray-800 to-blue-600 text-transparent bg-clip-text">Task</h1>
+    </a>
+
+    <div>
+
+    </div>
 </header>
--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import {resolve} from 'node:path'
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -11,7 +12,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias:{
+			'$components': resolve('./src/components'),
+			'$assets': resolve('./src/assets')
+		}
 	}
 };
 
