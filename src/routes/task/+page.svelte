7bb917bf5b73969d7ee7df0e5d7313d175786f37
Abdoul Aziz BALDE <baldaazi99@gmail.com>
2025-04-22 22:53:07 +0200

    fetch data

--- src/routes/task/+page.svelte
+++ src/routes/task/+page.svelte
@@ -1,3 +1,16 @@
+<script lang="ts">
+    let data:{userId: number; id: number; title:string; completed:boolean}[] = []
+
+    async function getTodos(){
+        const reponse = await fetch(`https://jsonplaceholder.typicode.com/todos/`)
+        data = await reponse.json()
+    }
+
+    getTodos()
+    
+</script>
+
 <div class="min-h-screen bg-blue-200">
+    <pre>{JSON.stringify(data, null, 2)}</pre>
 
 </div>
