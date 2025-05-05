8e3165c31fd60806f48c50195ef80abaaeb6e063
Abdoul Aziz BALDE <baldaazi99@gmail.com>
2025-05-05 19:46:09 +0200

    prosemirror

---
+++ src/routes/(app)/projects/writer/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { EditorState } from "prosemirror-state";
+	import { EditorView } from "prosemirror-view";
+	import { Schema } from "prosemirror-model";
+	import { schema as basicSchema } from "prosemirror-schema-basic";
+	import { toggleMark } from "prosemirror-commands";
+
+	let editorDiv: HTMLDivElement;
+	let view: EditorView;
+
+	$effect(() => {
+		const state = EditorState.create({
+			schema: basicSchema
+		});
+
+		view = new EditorView(editorDiv, {
+			state
+		});
+	});
+
+	function toggleBold() {
+		toggleMark(basicSchema.marks.strong)(view.state, view.dispatch);
+	}
+</script>
+
+<div>
+	<button onclick={toggleBold}>Bold</button>
+</div>
+<div bind:this={editorDiv} class="border p-2 mt-2 h-full"></div>
+
