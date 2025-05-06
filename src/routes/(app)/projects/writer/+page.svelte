7718a134f2477909901a02f00765d22333e9791c
Abdoul Aziz BALDE <baldaazi99@gmail.com>
2025-05-06 20:52:34 +0200

    text transform

--- src/routes/(app)/projects/writer/+page.svelte
+++ src/routes/(app)/projects/writer/+page.svelte
@@ -1,20 +1,34 @@
 <script lang="ts">
 	import { EditorState } from "prosemirror-state";
 	import { EditorView } from "prosemirror-view";
-	import { Schema } from "prosemirror-model";
 	import { schema as basicSchema } from "prosemirror-schema-basic";
-	import { toggleMark } from "prosemirror-commands";
+	import { toggleMark, baseKeymap } from "prosemirror-commands";
+	import {undo, redo, history} from "prosemirror-history"
+	import {keymap} from "prosemirror-keymap"
+	import { Bold, Redo, Undo } from "@lucide/svelte";
 
 	let editorDiv: HTMLDivElement;
 	let view: EditorView;
 
 	$effect(() => {
 		const state = EditorState.create({
-			schema: basicSchema
+			schema: basicSchema,
+			
+			plugins: [
+						history(),
+						keymap({"Mod-z": undo, "Mod-y": redo}),
+						keymap(baseKeymap)
+					]
 		});
 
 		view = new EditorView(editorDiv, {
-			state
+			state,
+			dispatchTransaction(transaction) {
+				console.log("Document size went from", transaction.before.content.size,
+							"to", transaction.doc.content.size)
+				let newState = view.state.apply(transaction)
+				view.updateState(newState)
+			}
 		});
 	});
 
@@ -23,8 +37,12 @@
 	}
 </script>
 
-<div>
-	<button onclick={toggleBold}>Bold</button>
+<div class="flex flex-col">
+	<div class="flex border">
+		<button onclick={undo} aria-label="undo"><Undo/></button>
+		<button onclick={redo} aria-label="redo"><Redo/></button>
+	</div>
+	<button onclick={toggleBold} aria-label="bold"><Bold/></button>
 </div>
 <div bind:this={editorDiv} class="border p-2 mt-2 h-full"></div>
 
