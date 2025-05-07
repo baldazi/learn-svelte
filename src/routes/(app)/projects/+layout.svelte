0c6dc321c16b0916111e5c20bb02fa6854aaa905
Abdoul Aziz BALDE <baldaazi99@gmail.com>
2025-05-08 01:13:46 +0200

    add konva.js

--- src/routes/(app)/projects/+layout.svelte
+++ src/routes/(app)/projects/+layout.svelte
@@ -8,6 +8,7 @@
             <ul class="flex flex-col gap-2 p-2">
                 <li class="border text-center"><a href="/projects/pdfviewer">PDF Viewer</a></li>
                 <li class="border text-center"><a href="/projects/writer">writer</a></li>
+                <li class="border text-center"><a href="/projects/design">design</a></li>
             </ul>
         </nav>
     </aside>
---
+++ src/routes/(app)/projects/design/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import Konva from 'konva';
+
+    let containerDiv: HTMLDivElement
+
+    $effect(() => {
+        const stage = new Konva.Stage({
+            container: containerDiv,
+            width: window.innerWidth,
+            height: window.innerHeight,
+        });
+
+        const layer = new Konva.Layer();
+        stage.add(layer);
+
+        const circle = new Konva.Circle({
+            x: stage.width() / 2,
+            y: stage.height() / 2,
+            radius: 70,
+            fill: 'red',
+            stroke: 'black',
+            strokeWidth: 4,
+            draggable: true,
+        });
+        // add cursor styling
+        circle.on('mouseover', function () {
+        document.body.style.cursor = 'pointer';
+        });
+        circle.on('mouseout', function () {
+        document.body.style.cursor = 'default';
+        });
+
+        layer.add(circle);
+    })
+    
+
+    
+</script>
+
+<div class="flex flex-wrap">
+    <div bind:this={containerDiv} class="w-[72mm] border rounded"></div>
+</div>
+
