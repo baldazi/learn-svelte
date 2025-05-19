f3b850489c43469248d76dff0e3b987a0d1f841b
Abdoul Aziz BALDE <baldaazi99@gmail.com>
2025-05-19 23:28:49 +0200

    adding chadcnui

--- src/routes/(app)/projects/writer/+page.svelte
+++ src/routes/(app)/projects/writer/+page.svelte
@@ -6,10 +6,11 @@
 	import {undo, redo, history} from "prosemirror-history"
 	import {keymap} from "prosemirror-keymap"
 	import { Bold, Indent, Italic, Outdent, Redo, Subscript, Superscript, Text, Underline, Undo } from "@lucide/svelte";
-	import ContextMenu from "$components/contextMenu.svelte";
 	import tippy from 'tippy.js';
 	import 'tippy.js/dist/tippy.css'; 
 	import type { Action } from "svelte/action";
+	import { Button } from "$lib/components/ui/button";
+	import * as Menubar from "$lib/components/ui/menubar";
 
 	const tooltip: Action<HTMLElement, Object> = (node , params) => {
 		  $effect(() => {
@@ -49,10 +50,101 @@
 	}
 </script>
 
+<Menubar.Root>
+	<Menubar.Menu>
+	  <Menubar.Trigger>File</Menubar.Trigger>
+	  <Menubar.Content>
+		<Menubar.Item>
+		  New Tab <Menubar.Shortcut>⌘T</Menubar.Shortcut>
+		</Menubar.Item>
+		<Menubar.Item>
+		  New Window <Menubar.Shortcut>⌘N</Menubar.Shortcut>
+		</Menubar.Item>
+		<Menubar.Item>New Incognito Window</Menubar.Item>
+		<Menubar.Separator />
+		<Menubar.Sub>
+		  <Menubar.SubTrigger>Share</Menubar.SubTrigger>
+		  <Menubar.SubContent>
+			<Menubar.Item>Email link</Menubar.Item>
+			<Menubar.Item>Messages</Menubar.Item>
+			<Menubar.Item>Notes</Menubar.Item>
+		  </Menubar.SubContent>
+		</Menubar.Sub>
+		<Menubar.Separator />
+		<Menubar.Item>
+		  Print... <Menubar.Shortcut>⌘P</Menubar.Shortcut>
+		</Menubar.Item>
+	  </Menubar.Content>
+	</Menubar.Menu>
+	<Menubar.Menu>
+	  <Menubar.Trigger>Edit</Menubar.Trigger>
+	  <Menubar.Content>
+		<Menubar.Item>
+		  Undo <Menubar.Shortcut>⌘Z</Menubar.Shortcut>
+		</Menubar.Item>
+		<Menubar.Item>
+		  Redo <Menubar.Shortcut>⇧⌘Z</Menubar.Shortcut>
+		</Menubar.Item>
+		<Menubar.Separator />
+		<Menubar.Sub>
+		  <Menubar.SubTrigger>Find</Menubar.SubTrigger>
+		  <Menubar.SubContent>
+			<Menubar.Item>Search the web</Menubar.Item>
+			<Menubar.Separator />
+			<Menubar.Item>Find...</Menubar.Item>
+			<Menubar.Item>Find Next</Menubar.Item>
+			<Menubar.Item>Find Previous</Menubar.Item>
+		  </Menubar.SubContent>
+		</Menubar.Sub>
+		<Menubar.Separator />
+		<Menubar.Item>Cut</Menubar.Item>
+		<Menubar.Item>Copy</Menubar.Item>
+		<Menubar.Item>Paste</Menubar.Item>
+	  </Menubar.Content>
+	</Menubar.Menu>
+	<Menubar.Menu>
+	  <Menubar.Trigger>View</Menubar.Trigger>
+	  <Menubar.Content>
+		<Menubar.CheckboxItem 
+		  >Always Show Bookmarks Bar</Menubar.CheckboxItem
+		>
+		<Menubar.CheckboxItem>
+		  Always Show Full URLs
+		</Menubar.CheckboxItem>
+		<Menubar.Separator />
+		<Menubar.Item inset>
+		  Reload <Menubar.Shortcut>⌘R</Menubar.Shortcut>
+		</Menubar.Item>
+		<Menubar.Item inset>
+		  Force Reload <Menubar.Shortcut>⇧⌘R</Menubar.Shortcut>
+		</Menubar.Item>
+		<Menubar.Separator />
+		<Menubar.Item inset>Toggle Fullscreen</Menubar.Item>
+		<Menubar.Separator />
+		<Menubar.Item inset>Hide Sidebar</Menubar.Item>
+	  </Menubar.Content>
+	</Menubar.Menu>
+	<Menubar.Menu>
+	  <Menubar.Trigger>Profiles</Menubar.Trigger>
+	  <Menubar.Content>
+		<Menubar.RadioGroup>
+		  <Menubar.RadioItem value="andy">Andy</Menubar.RadioItem>
+		  <Menubar.RadioItem value="benoit">Benoit</Menubar.RadioItem>
+		  <Menubar.RadioItem value="Luis">Luis</Menubar.RadioItem>
+		</Menubar.RadioGroup>
+		<Menubar.Separator />
+		<Menubar.Item inset>Edit...</Menubar.Item>
+		<Menubar.Separator />
+		<Menubar.Item inset>Add Profile...</Menubar.Item>
+	  </Menubar.Content>
+	</Menubar.Menu>
+  </Menubar.Root>
 
-<ContextMenu/>
-
+<div>
+	<Button>Click me</Button>
+</div>
 <div class="flex gap-1 shadow-2xl p-2 items-center h-12 border rounded mx-1">
+	
 	<div class="flex gap-1.5">
 		<button onclick={undo} use:tooltip={{content :"undo"}} aria-label="undo"><Undo/></button>
 		<button onclick={redo} use:tooltip={{content :"redo"}}><Redo/></button>
@@ -79,8 +171,8 @@
 	class="mx-auto border-2 p-2 mt-2 w-[210mm] h-[297mm] mx-1 border-gray-500 shadow-2xl rounded-xs"></div>
 
 
-<style>
-	.ProseMirror-focused .ProseMirror-gapcursor{
-    	outline: none;
-	}
-</style>
+<div class="fixed bg-gray-50 border border-gray-500/20 bottom-0 z-10 inset-x-0">
+rrrrrrrrrrrrrrrrrrrrrrrrrr
+</div>
+
+
